b217c928847985c94308b46c3560732abfd10249
iamrehan07 <92092421+iamrehan07@users.noreply.github.com>
2022-07-05 23:29:53 +0530

    Update action.sh

--- action.sh
+++ action.sh
@@ -1,5 +1,5 @@
 branch=main
-git clone -b $branch https://github.com/Randi356/Ultroid /root/Randi356
+git clone -b main https://github.com/Randi356/Ultroid /root/Randi356
 cp ultroid/.env /root/Randi356/.env
 cd /root/Randi356
 docker build . --rm --force-rm --compress --pull --file Dockerfile -t ultroid
